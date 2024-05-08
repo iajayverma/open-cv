e195be021107d05afeed5733fe15e3ed29c38859
Ajay Verma <aviverma.cse@gmail.com>
2024-05-08 17:38:26 +0530

    first commit

---
+++ session_1.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+
+#read image
+image_path=os.path.join('.','C:\Users\Developer\Desktop\jiten bhai tehshil/DSC_6694.JPG')
+
+img=cv2.imread(image_path)
+
+# write image
+
+cv2.imwrite()
